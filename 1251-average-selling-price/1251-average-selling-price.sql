55dd6b89eb33987df828204aa3fd8ba57a6ba40e
Trigun <t.trigun.2005@gmail.com>
2026-07-07 12:32:45 +0530

    Time: 880 ms (36.4%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,8 @@
+#with cte as( 
+select p.product_id, 
+    round(ifnull(sum(u.units*price)/sum(units),0),2) as average_price
+from prices p 
+left join unitssold u 
+on p.product_id=u.product_id
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
