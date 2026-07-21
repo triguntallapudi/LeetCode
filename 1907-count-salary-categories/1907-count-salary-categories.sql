fdfb04053002267158e99ef31fe08f279a407d7b
Trigun <t.trigun.2005@gmail.com>
2026-07-21 10:11:35 +0530

    Time: 1888 ms (9.99%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement belo
+select
+    'Low Salary' as category,
+    count(income) accounts_count
+from accounts 
+where income<20000
+
+union all
+
+select
+    'Average Salary' as category,
+    count(income) accounts_count
+from accounts 
+where income between 20000 and 50000
+
+union all
+
+select
+    'High Salary' as category,
+    count(income) accounts_count
+from accounts 
+where income>50000
