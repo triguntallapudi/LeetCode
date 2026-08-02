e42f11cbd4651a5c4dc403750772d80358343107
Tallapudi Trigun <t.trigun.2005@gmail.com>
2026-08-03 00:01:48 +0530

    Time: 3 ms (29.55%), Space: 12.4 MB (56.36%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        sum=0
+        for i in range(1,min(a,b)+1):
+            if a%i==0 and b%i==0:
+                sum+=1
+        return sum
+        
