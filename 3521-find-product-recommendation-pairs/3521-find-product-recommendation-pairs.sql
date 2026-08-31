acb740620f9bdec1d1eee4dbcead5487579310b1
Tallapudi Trigun <t.trigun.2005@gmail.com>
2026-08-31 18:29:56 +0530

    Time: 4360 ms (5.06%), Space: 0B (100%) - LeetHub

---
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+select
+    p1.product_id as product1_id,
+    p2.product_id as product2_id,
+    i1.category as product1_category,
+    i2.category as product2_category,
+    count(distinct p1.user_id) as customer_count
+from productpurchases p1 
+join productpurchases p2
+    on p1.user_id=p2.user_id
+    and p1.product_id < p2.product_id
+join productinfo i1
+    on p1.product_id=i1.product_id
+join productinfo i2
+    on p2.product_id=i2.product_id
+group by
+    p1.product_id,
+    p2.product_id,
+    i1.category,
+    i2.category
+having customer_count>=3
+order by
+    customer_count desc,
+    product1_id,
+    product2_id
+
+
